0dcb7fe1e8c8cae647e0363ab35e7bfef0e1ab00
Kapitonov Roman <kapitroman@yandex.ru>
2020-10-03 16:42:06 +0300

    верстка шапки сайта

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,20 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var mainNav = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  mainNav.classList.remove('main-nav--nojs');
+  mainNav.classList.remove('main-nav--opened');
+  mainNav.classList.add('main-nav--closed');
+
+  navToggle.addEventListener('click', function () {
+    if (mainNav.classList.contains('main-nav--closed')) {
+      mainNav.classList.remove('main-nav--closed');
+      mainNav.classList.add('main-nav--opened');
+    } else {
+      mainNav.classList.remove('main-nav--opened');
+      mainNav.classList.add('main-nav--closed');
+    }
+  });
+})();
